f398a754e958ae138aecbac622fe6c3849382a41
tomerlir <tomerlir95@gmail.com>
2025-11-02 18:38:06 +0100

    Further optimization on CSS and fonts loading

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { asyncCssPlugin } from "./vite-plugin-async-css";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -8,7 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
-  plugins: [react()],
+  plugins: [react(), asyncCssPlugin()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
